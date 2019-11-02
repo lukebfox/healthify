363c7d154f95501180dd3a0f4dbb03dbaf94a9bd
acb17rb <rbagdonas1@sheffield.ac.uk>
2019-11-02 19:53:43 +0000

    add fetchGoals method

--- src/backend/timed-updates.js
+++ src/backend/timed-updates.js
@@ -5,8 +5,42 @@
  *  2.1 
  * 3. Send Insert/Update query HTTP Post
  */
-const cron = require("node-cron");
-console.log("cron setup");
-cron.schedule("*/2 * * * * *", function() {
-    console.log("Testing cron corns");
-});
+// const cron = require("node-cron");
+// console.log("cron setup");
+// cron.schedule("*/2 * * * * *", function() {
+//     console.log("Testing cron corns");
+// });
+
+const fetch = require("node-fetch");
+const query = `
+query MyQuery {
+    goals {
+      allowed_fails
+      amount
+      decay
+      fail
+      id
+      metrics
+      provider
+    }
+  }
+`;
+const url = "https://hs-goals.herokuapp.com/v1/graphql";
+
+async function fetchGoals(){
+    let response = await fetch(url, { 
+        method: 'POST',
+        Accept: 'api_version=2',
+        'Content-Type': 'application/graphql',
+        body: JSON.stringify({ query })
+      });
+      response = await response.json();
+      response = response.data.goals;
+      return response;
+}
+
+async function main() {
+    console.log(await fetchGoals());
+}
+
+main();
