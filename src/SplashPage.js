e2f522fc1365840a48817da8a2191e2552e7ff09
HK <17625822+darkbluee@users.noreply.github.com>
2019-11-03 10:40:14 +0000

    Splash page in

---
+++ src/SplashPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./splash.css"
+
+export default function SplashPage() {
+    
+  
+    return (
+        <div class="bgimg-1">
+        <div class="caption">
+          <span class="border"> <a href="/goals"><button class="button">I want to become healthier and change my habits</button></a> </span>
+         
+        </div>
+      </div>
+      
+      
+    );
+  }
+  
